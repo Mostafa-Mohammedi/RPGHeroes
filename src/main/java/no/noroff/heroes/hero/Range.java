942a60235ceb02e1f6908d752b00de9eb8bb6815
Mostafa-Mohammedi <121897256+Mostafa-Mohammedi@users.noreply.github.com>
2023-01-15 00:50:13 +0100

    Finished the range class with all the function and method.

--- src/main/java/no/noroff/heroes/hero/Range.java
+++ src/main/java/no/noroff/heroes/hero/Range.java
@@ -43,30 +43,73 @@ public class Range extends Hero {
     @Override
     public void levelUp(double level_increase) {
 
+        double total_strength_Increase = (level_increase * strength_increase_each_level) + levelAttribute.getStrength();
+        double total_dexterity_Increase = (level_increase * dexterity_increase_each_level) + levelAttribute.getDexterity();
+        double total_intelligence_Increase = (level_increase * intelligence_increase_each_level) + levelAttribute.getIntelligence();
+
+        levelAttribute.setStrength(total_strength_Increase) ;
+        levelAttribute.setDexterity(total_dexterity_Increase);
+        levelAttribute.setIntelligence(total_intelligence_Increase);
+
+        level += level_increase;
+        System.out.println("total level increase: " + total_strength_Increase);
+
     }
 
     @Override
     public void damage() {
+        total_range_damage = WeaponDamage * (1+ (levelAttribute.getStrength()/100));
+        System.out.println("total damage: " + total_range_attribute);
 
     }
 
     @Override
     public String equip_armor(String armor_type, double armor_level) throws InvalidWeaponType {
-        return null;
+        armor_attribute = armor_level;
+
+        for (String armor : Valid_Armor_type) {
+
+            if(armor.toUpperCase().equals(armor_type) && armor_level == level){
+
+                return "equipment: " + armor_type + " has been added to " + name;
+            }
+        }
+
+        throw new InvalidWeaponType( armor_type + " cannot be used for " + this.getClass().getSimpleName() + " hero ");
     }
+
 
     @Override
     public String equip_weapon(String weapon_type, double weapon_level) throws InvalidWeaponType {
-        return null;
+        //WeaponDamage += weapon_level;
+
+        for (String weapon : valid_Weapon_type) {
+
+            if(weapon.toUpperCase().equals(weapon_type) && weapon_level == level){
+                System.out.println("equipment: " + weapon_type + " has been added to " + name);
+                return "equipment: " + weapon_type + " has been added to " + name;
+            }
+        }
+        throw new InvalidWeaponType(weapon_type + " cannot be used for " + name);
     }
 
     @Override
     public void totalAttributes() {
+        total_range_attribute = (levelAttribute.getStrength() +
+                levelAttribute.getDexterity() +
+                levelAttribute.getIntelligence()) +
+                (WeaponDamage + armor_attribute);
 
     }
 
     @Override
     public String display() {
-        return null;
+        return "name: " + name +
+                " class: " + this.getClass().getSimpleName() +
+                " level: " + level +
+                " strength: " +  levelAttribute.getStrength() +
+                " dexterity: " +levelAttribute.getDexterity() +
+                " intelligence: " + levelAttribute.getIntelligence() +
+                " Damage " + total_range_damage;
     }
 }
